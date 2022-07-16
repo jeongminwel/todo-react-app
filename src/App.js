b03db68a7e854068acbad2bec481991f8ffadf21
jeongminwel <95.welJm@gmail.com>
2022-07-16 22:20:37 +0900

    Todo List 추가 삭제 수정 작업

--- src/App.js
+++ src/App.js
@@ -1,30 +1,55 @@
 import React from "react";
 import Todo from "./Todo";
+import AddTodo from "./AddTodo.js";
+import { Paper, List, Container } from "@material-ui/core";
 import "./App.css";
-import { List, Paper } from '@material-ui/core';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items : [
-        { id:0, title: "jeongmin", done: true },    
-        { id:1, title: "jeongmin1", done: false },
-      ],
+      items: [],
     };
   }
 
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length; 
+    item.done = false; 
+    thisItems.push(item); 
+    this.setState({ items: thisItems }); 
+    console.log("items : ", this.state.items);
+  };
+
+  delete = (item) => {
+    const thisItems = this.state.items;
+    console.log("Before Update Items : ", this.state.items);
+    const newItems = thisItems.filter((e) => e.id !== item.id);
+    this.setState({ items: newItems }, () => {
+      // 디버깅 콜백
+      console.log("Update Items : ", this.state.items);
+    });
+  };
+
   render() {
     var todoItems = this.state.items.length > 0 && (
-      <Paper style={{ margin : 16}}>
+      <Paper style={{ margin: 16 }}>
         <List>
-          {this.state.items.map((item, idx) => {
-              return <Todo item={item} key={item.id} />
-          })}
-        </List>    
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} delete={this.delete} />
+          ))}
+        </List>
       </Paper>
     );
-    return <div className="App">{todoItems}</div>
+
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    );
   }
 }
 
